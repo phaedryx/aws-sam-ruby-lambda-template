72176b19855d7bc96f4a09e812c1defcc661f19b
Tad Thorley <phaedryx@gmail.com>
2020-05-21 20:07:45 -0600

    adds more configuration and examples

--- {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}.rb
+++ {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}.rb
@@ -3,12 +3,23 @@
 require 'logger'
 
 def lambda_handler(event:, context:)
-  available_gems = system("gem list --local")
+  available_gems = `gem list --local`
   {
     event: event,
-    context: context,
-    available_gems: available_gems,
+    context: {
+      function_name: context.function_name,
+      function_version: context.function_version,
+      invoked_function_arn: context.invoked_function_arn,
+      memory_limit_in_mb: context.memory_limit_in_mb,
+      aws_request_id: context.aws_request_id,
+      log_group_name: context.log_group_name,
+      log_stream_name: context.log_stream_name,
+      deadline_ms: context.deadline_ms,
+      identity: context.identity,
+      client_context: context.client_context,
+    },
     environment: ENV.to_h
+    available_gems: available_gems.split("\n"),
   }
 rescue => err
   logger = Logger.new($stdout)
@@ -16,6 +27,5 @@ rescue => err
     error: err.message,
     backtrace: err.backtrace,
     event: event,
-    context: context,
   )
 end
